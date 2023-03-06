69c9fd73ebdadc0fcd015fb98b0c7f29903f90d4
Roshan Kafle <121845834+Kafle33@users.noreply.github.com>
2023-03-06 19:50:53 +0545

    Add files via upload
    found this on my old laptop

---
+++ My website-2019/style.css
@@ -0,0 +1,59 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+  
+  header {
+    background-color: #333;
+    color: #fff;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+  }
+  
+  nav ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+  }
+  
+  nav li {
+    margin-right: 20px;
+  }
+  
+  nav a {
+    color: #fff;
+    text-decoration: none;
+    font-weight: bold;
+  }
+  
+  main {
+    margin: 20px;
+  }
+  
+  section {
+    margin-bottom: 50px;
+  }
+  
+  h2 {
+    font-size: 32px;
+    margin-bottom: 20px;
+  }
+  
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  
+  li h3 {
+    margin-bottom: 10px;
+  }
+  
+  a {
+    display: inline-block;
+    padding: 10px
+  
